e6ae07d46e6d3c20036fa54c14c81a93e90ac2a7
Daniel Jäkel <danieljakob.jaekel@stud.hs-kempten.de>
2021-06-17 21:36:52 +0200

    Add VideoOutput for debugging

---
+++ Camera/cameraOutput.py
@@ -0,0 +1,28 @@
+# Own class for cameraOutput with own thread
+# Only for debugging
+
+import cv2
+import numpy as np
+
+# import concurrent.futures
+from threading import Thread
+import time
+
+
+class VideoOutput:
+    def __init__(self, frame=None):
+        self.frame = frame
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.show, args=()).start()
+        return self
+
+    def show(self):
+        while not self.stopped:
+            cv2.imshow("Video", self.frame)
+            if cv2.waitKey(1) == ord("q"):
+                self.stopped = True
+
+    def stop(self):
+        self.stopped = True

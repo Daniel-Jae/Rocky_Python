6f072d6a1bb233704914eb1fb2817694d50ae39e
Daniel Jäkel <danieljakob.jaekel@stud.hs-kempten.de>
2021-06-17 20:42:04 +0200

    Updated CameraStream

--- Camera/CameraStream.py
+++ Camera/CameraStream.py
@@ -2,25 +2,17 @@ import cv2
 import numpy as np
 
 # import concurrent.futures
-import threading
+from threading import Thread
 import time
 
 
 class VideoStream:
-    def __init__(self, resolution=(320, 240), framerate=60, camera=0):
-        # initialize the camera and stream
-        self.camera = camera
-        self.resolution = resolution
-        self.framerate = framerate
-        self.realFramerate = framerate
-        # time between frames in milliseconds
-        ### self.timePerFrame = 1000 / framerate
+    def __init__(self, camera=0, framerate=60):
+        self.stream = cv2.VideoCapture(camera)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
         self.framesSinceLastRequest = 0
-
-        # initialize the frame and the variable used to indicate
-        # if the thread should be stopped
-        self.frame = None
-        self.stopped = True
+        self.framerate = framerate
 
     def calculateFPS():
         # Calculate the "real" Frames of the Camera
@@ -43,63 +35,45 @@ class VideoStream:
         return fps
 
     def start(self):
-        # start the thread to read frames from the video stream
-        if self.stopped:
-            self.stopped = False
-            t1 = threading.Thread(target=self.update)
-            t1.start()
-            # Potentielle Alternativen
-            # 1
-            # add "t1.daemon = True" before "t1.start()"
-            # 2
-            # thread_pool = concurrent.futures.ThreadPoolExecutor()
-            # thread_pool.submit(self.update)
-            # time.sleep(2.0)
-            return self
+        Thread(target=self.get, args=()).start()
+        return self
 
-    def update(self):
-        # get always a new picture as long as the thread is still alive
-        video = cv2.VideoCapture(self.camera)
-        if not video.isOpened():
-            print("Cannot open camera")
-            return
-        try:
-            while True:
-                # print("new pic")
-                # grab the frame from the stream and clear the stream in
-                # preparation for the next frame
-                ret, frame = video.read()
-                # ret checks if you could actually get a frame
-                if ret:
-                    self.frame = frame
-                    cv2.imshow("image", self.frame)
-                self.newFrame = True
+    def get(self):
+        while not self.stopped:
+            if not self.grabbed:
+                self.stop()
+            else:
                 self.framesSinceLastRequest += 1
-
-                # if the thread indicator variable is set, stop the thread
-                # and resource camera resources
-                if self.stopped:
-                    video.release()
-                    print("Camera stopped.")
-                    return
-        except:
-            self.frame = None
+                (self.grabbed, self.frame) = self.stream.read()
+        print("Camera stopped.")
 
     def read(self):
         # return the newest frame
-        self.newFrame = False
+        framesSinceLastRequest = self.framesSinceLastRequest
+
         self.framesSinceLastRequest = 0
 
-        cv2.imshow("image", self.frame)
+        # cv2.imshow("image", self.frame)
 
-        return self.frame
+        return (self.frame, framesSinceLastRequest)
 
     def stop(self):
-        # indicate that the thread should be stopped
         self.stopped = True
         self.newFrame = False
         self.frame = None
-        self.counter.stop()
+
+
+def show(source=0):
+
+    video_getter = VideoStream(camera=source).start()
+
+    while True:
+        if (cv2.waitKey(1) == ord("q")) or video_getter.stopped:
+            video_getter.stop()
+            break
+
+        frame, amountOfFrames = video_getter.get()
+        cv2.imshow("Video", frame)
 
 
 if __name__ == "__main__":
